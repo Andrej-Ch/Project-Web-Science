2f2bab78b54801ad7bee70b0f368d9777f7e14c9
Andrej Ch <candrej@ymail.com>
2017-05-06 13:22:29 +0200

    Creates a list of question IDs
    Query can be generated here https://api.stackexchange.com/docs/questions

---
+++ QuestionIDToCsv.java
@@ -0,0 +1,90 @@
+package StackOverflow;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.URL;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * Created by Andro on 05-May-17.
+ */
+public class QuestionIDToCsv {
+
+
+    private static String readUrl(String query) throws IOException {
+
+        URL url = new URL(query);
+        BufferedReader in = new BufferedReader(new InputStreamReader(new GZIPInputStream(url.openStream())));
+        String line;
+        StringBuffer content = new StringBuffer();
+        while ((line = in.readLine()) != null) {
+            content.append(line);
+        }
+        return String.valueOf(content);
+    }
+
+    static class Item {
+        String accepted_answer_id;
+        String answer_count;
+        String answers;
+        String body;
+        String closed_date;
+        //String closed_details (closed_details);
+        String comment_count;
+        String comments;
+        String creation_date;
+        String delete_vote_count;
+        String down_vote_count;
+        String downvoted;
+        String favorite_count;
+        String favorited;
+        String is_answered;
+        String last_activity_date;
+        String last_edit_date;
+        //String last_editor (shallow_user);
+        String link;
+        String locked_date;
+        //String migrated_from (migration_info);
+        //String migrated_to (migration_info);
+        //String notice (notice);
+        //String owner (shallow_user);
+        String protected_date;
+        String question_id;
+        String reopen_vote_count;
+        String score;
+        String share_link;
+        String tags;
+        String title;
+        String up_vote_count;
+        String upvoted;
+        String view_count;
+
+    }
+
+    static class Page {
+        List<Item> items;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String query = "https://api.stackexchange.com/2.2/questions?pagesize=10&order=desc&min=200&sort=votes&site=stackoverflow&filter=!2.dt38CU)6yRIbWVfDpQc";
+
+        String json = readUrl(query);
+        Page page = new Gson().fromJson(json, Page.class);
+
+        PrintWriter pw = new PrintWriter(new File("Question_IDs.csv"));
+        StringBuilder sb = new StringBuilder();
+
+        for (Item item : page.items) {
+            sb.append(item.question_id);
+            sb.append(",");
+            sb.append('\n');
+        }
+
+        pw.write(sb.toString());
+        pw.close();
+        System.out.println("List of question IDs saved as \"Question_IDs.csv\"");
+    }
+}
